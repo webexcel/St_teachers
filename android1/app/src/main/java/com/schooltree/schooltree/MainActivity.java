42bb44e91739c927f59816d8a4cb0c510f1ae16a
Webexcel Technologies <epraburajan@gmail.com>
2024-07-11 13:29:48 +0530

    11-07

--- android1/app/src/main/java/com/schooltree/schooltree/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.schooltree.schooltree;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -23,6 +23,7 @@ export const environment = {
             "status": "0",
             "UserId": "2063"
         },
+
         {
             "menu_id": "2",
             "menu_name": "homework",
@@ -86,6 +87,7 @@ export const environment = {
             "status": "0",
             "UserId": "2063"
         },
+<<<<<<< Updated upstream
         {
             "menu_id": "9",
             "menu_name": "Staff",
@@ -104,6 +106,27 @@ export const environment = {
             "status": "0",
             "UserId": "2063"
         },
+=======
+
+        // {
+        //     "menu_id": "9",
+        //     "menu_name": "Staff",
+        //     "menu_link": "/staff",
+        //     "menu_icon": "https://demo.schooltree.in/uploads/mobileApp_icons/staff.svg",
+        //     "menu_order": "9",
+        //     "status": "0",
+        //     "UserId": "2063"
+        // },
+        // {
+        //     "menu_id": "10",
+        //     "menu_name": "Staff Group",
+        //     "menu_link": "/staff-group",
+        //     "menu_icon": "https://demo.schooltree.in/uploads/mobileApp_icons/staffgroup.svg",
+        //     "menu_order": "10",
+        //     "status": "0",
+        //     "UserId": "2063"
+        // },
+>>>>>>> Stashed changes
         // {
         //     "menu_id": "11",
         //     "menu_name": "Fee-report",
@@ -113,6 +136,7 @@ export const environment = {
         //     "status": "0",
         //     "UserId": "2063"
         // },
+<<<<<<< Updated upstream
         {
             "menu_id": "12",
             "menu_name": "Timetable",
@@ -221,5 +245,106 @@ export const environment = {
             "status": "0",
             "UserId": "2063"
         },
+=======
+        // {
+        //     "menu_id": "12",
+        //     "menu_name": "Timetable",
+        //     "menu_link": "/timetable",
+        //     "menu_icon": "https://demo.schooltree.in/uploads/mobileApp_icons/timetable.svg",
+        //     "menu_order": "12",
+        //     "status": "0",
+        //     "UserId": "2063"
+        // },
+        // {
+        //     "menu_id": "13",
+        //     "menu_name": "Add Remarks",
+        //     "menu_link": "/reports",
+        //     "menu_icon": "https://demo.schooltree.in/uploads/mobileApp_icons/edit_remarks.svg",
+        //     "menu_order": "13",
+        //     "status": "0",
+        //     "UserId": "2063"
+        // },
+        // {
+        //     "menu_id": "14",
+        //     "menu_name": "View Remarks",
+        //     "menu_link": "/view-remarks",
+        //     "menu_icon": "https://demo.schooltree.in/uploads/mobileApp_icons/view_remarks.svg",
+        //     "menu_order": "14",
+        //     "status": "0",
+        //     "UserId": "2063"
+        // },
+        // {
+        //     "menu_id": "15",
+        //     "menu_name": "Parent Messages",
+        //     "menu_link": "/uploads",
+        //     "menu_icon": "https://demo.schooltree.in/uploads/mobileApp_icons/uploads.svg",
+        //     "menu_order": "15",
+        //     "status": "0",
+        //     "UserId": "2063"
+        // },
+        // {
+        //     "menu_id": "16",
+        //     "menu_name": "Class Fees Report",
+        //     "menu_link": "/class-student-fee-report",
+        //     "menu_icon": "https://demo.schooltree.in/uploads/mobileApp_icons/class-student-fee-report.svg",
+        //     "menu_order": "15",
+        //     "status": "0",
+        //     "UserId": "2063"
+        // },
+        // {
+        //     "menu_id": "17",
+        //     "menu_name": "Expences",
+        //     "menu_link": "/expences",
+        //     "menu_icon": "https://demo.schooltree.in/uploads/mobileApp_icons/expenses.svg",
+        //     "menu_order": "16",
+        //     "status": "0",
+        //     "UserId": "2063"
+        // },
+        // {
+        //     "menu_id": "18",
+        //     "menu_name": "Absentees",
+        //     "menu_link": "/absentees",
+        //     "menu_icon": "https://demo.schooltree.in/uploads/mobileApp_icons/absentees.svg",
+        //     "menu_order": "17",
+        //     "status": "0",
+        //     "UserId": "2063"
+        // },
+        // {
+        //     "menu_id": "19",
+        //     "menu_name": "Staff Free Period",
+        //     "menu_link": "/staff-free-period",
+        //     "menu_icon": "https://demo.schooltree.in/uploads/mobileApp_icons/stafffreeperiod.svg",
+        //     "menu_order": "18",
+        //     "status": "0",
+        //     "UserId": "2063"
+        // },
+        // {
+        //     "menu_id": "20",
+        //     "menu_name": "Teachers Report",
+        //     "menu_link": "/teachers-report",
+        //     "menu_icon": "https://demo.schooltree.in/uploads/mobileApp_icons/teachers-report.svg",
+        //     "menu_order": "19",
+        //     "status": "0",
+        //     "UserId": "2063"
+        // },
+        // {
+        //     "menu_id": "21",
+        //     "menu_name": "Mark Enter",
+        //     "menu_link": "/mark-enter",
+        //     "menu_icon": "https://demo.schooltree.in/uploads/mobileApp_icons/mark-enter.svg",
+        //     "menu_order": "20",
+        //     "status": "0",
+        //     "UserId": "2063"
+        // },
+        // {
+        //     "menu_id": "22",
+        //     "menu_name": "Gallery",
+        //     "menu_link": "/gallery",
+        //     "menu_icon": "https://demo.schooltree.in/uploads/mobileApp_icons/gallery.svg",
+        //     "menu_order": "21",
+        //     "status": "0",
+        //     "UserId": "2063"
+        // },
+>>>>>>> Stashed changes
     ]
 };
